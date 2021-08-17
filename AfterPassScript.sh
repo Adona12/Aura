5ccf6789a20ac94c9fa49152b5ef72023d6d1617
Adona12 <adonatesfaye99@gmail.com>
2021-08-17 08:55:10 -0400

    script

--- AfterPassScript.sh
+++ AfterPassScript.sh
@@ -7,21 +7,21 @@
 
 current_directory=$(pwd);
 
-for  release in    memcached-1.6.9 
+for  release in    memcached-1.4.22
 do  
     cd $1/$release;
     sudo make install;
     extract-bc memcached;
     cd ../..;
     cd output;
-    sudo mkdir -p after-load-only/$release;
-    cd after-load-only;
-    sudo chmod a+rwx $release;
+    # sudo mkdir -p after-load-only/$release;
+    # cd after-load-only;
+    # sudo chmod a+rwx $release;
     # cd ..;
 
-    # sudo mkdir -p after-store-only/$release;
-    # cd after-store-only;
-    # sudo chmod a+rwx $release;
+    sudo mkdir -p after-store-only/$release;
+    cd after-store-only;
+    sudo chmod a+rwx $release;
     # cd ..;
 
     # sudo mkdir -p after-nop/$release;
@@ -36,14 +36,14 @@ do
     for i in 8
     do
         cd output;
-        sudo mkdir -p after-load-only/$release/$i;
-        cd after-load-only/$release;
-        sudo chmod a+rwx $i;
+        # sudo mkdir -p after-load-only/$release/$i;
+        # cd after-load-only/$release;
+        # sudo chmod a+rwx $i;
         # cd ../..;
 
-        # sudo mkdir -p after-store-only/$release/$i;
-        # cd after-store-only/$release;
-        # sudo chmod a+rwx $i;
+        sudo mkdir -p after-store-only/$release/$i;
+        cd after-store-only/$release;
+        sudo chmod a+rwx $i;
         # cd ../..;
 
 
@@ -59,13 +59,13 @@ do
 
 
         cd $1/$release;
-         afterloadFile=/proj/proxyweb-PG0/adona/output/after-load-only/$release/$i; 
-        #   afterstoreFile=/proj/proxyweb-PG0/adona/output/after-store-only/$release/$i; 
+        #  afterloadFile=/proj/proxyweb-PG0/adona/output/after-load-only/$release/$i; 
+           afterstoreFile=/proj/proxyweb-PG0/adona/output/after-store-only/$release/$i; 
         #  afternopFile=/proj/proxyweb-PG0/adona/output/after-nop/$release/$i; 
         #  afterBothLoadAndStore==/proj/proxyweb-PG0/adona/output/after-both-load-and_store/$release/$i; 
 
-         load=$i;store=0;nop=0; llc memcached.bc --enable-emit-pass-with-extract-info --emit-and-extract-same-load-insts-ratio=${load} --emit-and-extract-same-store-insts-ratio=${store} --emit-and-extract-nop=${nop} --emit-pass-with-extract-info-path=${afterloadFile} -o memcachedLoadAfter_$i.o -filetype=obj;
-        #  load=0;store=$i;nop=0; llc memcached.bc --enable-emit-pass-with-extract-info --emit-and-extract-same-load-insts-ratio=${load} --emit-and-extract-same-store-insts-ratio=${store} --emit-and-extract-nop=${nop} --emit-pass-with-extract-info-path=${afterstoreFile} -o memcachedStoreAfter_$i.o -filetype=obj;
+        #  load=$i;store=0;nop=0; llc memcached.bc --enable-emit-pass-with-extract-info --emit-and-extract-same-load-insts-ratio=${load} --emit-and-extract-same-store-insts-ratio=${store} --emit-and-extract-nop=${nop} --emit-pass-with-extract-info-path=${afterloadFile} -o memcachedLoadAfter_$i.o -filetype=obj;
+         load=0;store=$i;nop=0; llc memcached.bc --enable-emit-pass-with-extract-info --emit-and-extract-same-load-insts-ratio=${load} --emit-and-extract-same-store-insts-ratio=${store} --emit-and-extract-nop=${nop} --emit-pass-with-extract-info-path=${afterstoreFile} -o memcachedStoreAfter_$i.o -filetype=obj;
         #  load=$i;store=$i;nop=1; llc memcached.bc --enable-emit-pass-with-extract-info --emit-and-extract-same-load-insts-ratio=${load} --emit-and-extract-same-store-insts-ratio=${store} --emit-and-extract-nop=${nop} --emit-pass-with-extract-info-path=${afternopFile} -o memcachedNopAfter_$i.o -filetype=obj;
         #  load=$i;store=$i;nop=0; llc memcached.bc --enable-emit-pass-with-extract-info --emit-and-extract-same-load-insts-ratio=${load} --emit-and-extract-same-store-insts-ratio=${store} --emit-and-extract-nop=${nop} --emit-pass-with-extract-info-path=${afterBothLoadAndStore} -o memcachedLoadAndStoreAfter_$i.o -filetype=obj
         cd ../..;
